b4ca3e660b922a5674cd245e8f76107e2793abb5
gerty <gerty33@gmail.com>
2018-12-05 16:33:33 -0600

    Stumped for now.

--- Day 05 - Alchemical Reduction/AoC2018-Day05.py
+++ Day 05 - Alchemical Reduction/AoC2018-Day05.py
@@ -12,7 +12,6 @@ with open('Day 5 puzzle input.txt', 'r') as f:  # open file for reading into pol
             break
         polymer.append(c)
 
-print(polymer)  # check for realistic input
 
 def collapse(poly):  # This function will collapse the entire text string to eliminate all opposite case neighbors
     activepoly = poly  # this loads the polymer string for the first time
@@ -22,12 +21,10 @@ def collapse(poly):  # This function will collapse the entire text string to eli
         done = True
         for p in range(1, len(activepoly)):  # start at one more than beginning, go until the end
             if activepoly[p] == str.swapcase(activepoly[p-1]):  # compare with previous character
-                print(activepoly[p-1] + activepoly[p])
                 nextpoly = nextpoly[:-1]  # shave off the end by one if we find a match, and don't add to it
                 done = False  # you did some change to the string, so you're not done
             else:
-                nextpoly.append(activepoly[p])  # in this case, just tack on the character
-        print(len(nextpoly))
+                nextpoly.append(activepoly[p])  # in this case, just tack on the unmatching character
         activepoly = nextpoly
     return activepoly
 
@@ -36,3 +33,6 @@ print(len(collapse(polymer)))
 # Saw length of nextPoly getting greater each time through. No bueno. Will think about this in the shower.
 # Shower worked, but realistic answer 3508 is now too low.
 # Next day, 11816 is too high, after I found an obvious bug. Let's see if I can find the not-so-obvious bugs.
+# 11815 is too high
+# 11744 is incorrect (tried ranging only to len(activepoly)-1)
+
