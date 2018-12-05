a27c2a29d340aeea0803377efaa831b82f231644
gerty <gerty33@gmail.com>
2018-12-05 15:54:28 -0600

    Fixed an errant 'poly' callout but still not a good answer to part 1.

--- Day 05 - Alchemical Reduction/AoC2018-Day05.py
+++ Day 05 - Alchemical Reduction/AoC2018-Day05.py
@@ -5,28 +5,26 @@
 
 polymer = []
 
-with open('Day 5 puzzle input.txt', 'r') as f:
+with open('Day 5 puzzle input.txt', 'r') as f:  # open file for reading into polymer, char by char
     while True:
         c = f.read(1)
-        if not c:
-            print("End of file")
+        if not c:  # EOF
             break
-        print("Read a character:", c)
         polymer.append(c)
 
 answer = 0  # This will be the answer
-print(polymer)
+print(polymer)  # check for realistic input
 
 
-def collapse(poly):
-    activepoly = poly
+def collapse(poly):  # This function will collapse the entire text string to eliminate all opposite case neighbors
+    activepoly = poly  # this loads the polymer string for the first time
     done = False
     while not done:
-        nextpoly = []
+        nextpoly = []  # this is our new iteration (but possibly not the last before we leave)
         nextpoly += poly[0]  # Always start with the first element loaded
         done = True
-        for p in range(1,len(activepoly)):  # start at one more than beginning
-            if poly[p] == str.swapcase(activepoly[p-1]):  # compare with previous character
+        for p in range(1, len(activepoly)):  # start at one more than beginning, go until the end
+            if activepoly[p] == str.swapcase(activepoly[p-1]):  # compare with previous character
                 nextpoly = nextpoly[:-1]  # shave off the end by one
                 done = False  # you did some change to the string, so you're not done
             else:
@@ -42,3 +40,4 @@ print(answer)
 
 # Saw length of nextPoly getting greater each time through. No bueno. Will think about this in the shower.
 # Shower worked, but realistic answer 3508 is now too low.
+# Next day, 11816 is too high, after I found an obvious bug. Let's see if I can find the not-so-obvious bugs.
