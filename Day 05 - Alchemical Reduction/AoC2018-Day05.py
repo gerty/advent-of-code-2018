4f1adcf0662dcf16694b2f9547b90c3cebea7953
gerty <gerty33@gmail.com>
2018-12-12 02:50:40 -0600

    Needed to protect for the len = 1 case. Part 1 works.

--- Day 05 - Alchemical Reduction/AoC2018-Day05.py
+++ Day 05 - Alchemical Reduction/AoC2018-Day05.py
@@ -15,16 +15,20 @@ with open('Day 5 puzzle input.txt', 'r') as f:  # open file for reading into pol
 # Now collapse the entire text string to eliminate all opposite case neighbors.
 
 result = [polymer[0]]  # Always start with the first element loaded into the result
-print('result=')
+print('result length = ')
 print(len(result))
 index = 1  # start evaluating at second location
 while index < len(polymer):  # go until the end
     result.append(polymer[index])  # tack on the next character
-    print('Result=')
+    print('Result length =')
     print(len(result))
-    r1 = result[-2]
-    r2 = str.swapcase(result[-1])
-    if r1 == r2:  # compare with last character of result
+    if (len(result)>1):  # Protect for len=1,0 case
+        r1 = result[-2]  # This should look at the second to last element
+    else:
+        r1 = ' '
+    r2 = str.swapcase(result[-1])  # This should look at the opposite case of the last element
+    if r1 == r2:  # compare the two
+        print(result[-2:])
         result = result[:-2]  # shave off the end (WHY is there a "IndexError: list index out of range" here???)
     index += 1  # increment polymer to the next char of interest
 
@@ -35,4 +39,4 @@ print(len(result))
 # 11815 is too high
 # 11744 is incorrect (tried ranging only to len(activepoly)-1)
 # Looking at the problem again, I think my issue is that I'm making multiple passes, instead of one thorough one.
-
+# Result 9348 is correct (needed to protect for the len = 1 case)
