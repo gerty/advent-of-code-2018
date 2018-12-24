6f2119d97e6037cc729cef2bc3bc6a5bbddc5fdd
gerty <gerty33@gmail.com>
2018-12-24 11:07:31 -0600

    Test code works for part 1 (day 7)

--- Day 07 - The Sum of Its Parts/AoC2018-Day07.py
+++ Day 07 - The Sum of Its Parts/AoC2018-Day07.py
@@ -15,25 +15,30 @@ for line in filedata:   # parse out the input
     allrules.append(parsed[1] + parsed[7])  # organize the rules in a two-character array
 
 for rule in allrules:  # cycle through each rule
+    if rule[0] not in steps:
+        steps[rule[0]] = 0  # create a new index if it doesn't exist
     if rule[1] not in steps:
-        steps[rule[1]] = 1  # create a new index if it doesn't exist
-    else:
-        steps[rule[1]] += 1  # increment each letter's index by one
+        steps[rule[1]] = 0  # create a new index if it doesn't exist
+    steps[rule[1]] += 1  # increment a step's index by one if it has a prereq
 
 answer = ''
 
 while steps:  # if there are still steps to be done
+    print(sorted(steps))
 
     for step in sorted(steps):  # cycle through prereq count dictionary, in alpha order
         if steps[step] == 0:  # if there are no more barriers to performing the step
-            answer += letter  # our answer became better
+            answer += step  # our answer became better by adding the step
 
             for rule in allrules:
-                if letter == rule[0]:  # "step 1 must be completed before step 2 can start"
-                    steps[rule[1]] -= 1;  # reduce the number of prereqs by one
-            print(allrules)
+                if step == rule[0]:  # "step 1 must be completed before step 2 can start"
+                    steps[rule[1]] -= 1  # reduce the number of prereqs by one
 
-            del steps[letter]  # but we need to delete the step so we don't do it again
+            del steps[step]  # but we need to delete the step so we don't do it again
+            break  # break out of the loop because need to resort
+        print(allrules)
+        print(steps)
+
 
 print(answer)
 
